d934ab3cd9959773ab0a2d4d1bfb51cd192f2783
Willy Eth <willyeth01@gmail.com>
2022-09-23 08:07:45 -0500

    refactor

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 import Dog from './Dog.js';
 import dogsData from './data.js';
 import dogs from './data.js';
-let isWaiting = false;
-let currentDogIndex = 0;
-let isLoved = 0;
-let notLoved = 0;
+ let isWaiting = false;
+ let currentDogIndex = 0;
+ let isLoved = 0;
+ let notLoved = 0;
+
+   
 let currentDog = new Dog(dogsData[currentDogIndex]);
-document.getElementById('accept-button').addEventListener('click', yes);
-document.getElementById('reject-button').addEventListener('click', no);
+document.getElementById('accept-button').addEventListener('click', () => {
+  currentDog.setMatchStatus(true);
+  getNewDog();
+});
+document.getElementById('reject-button').addEventListener('click', () => {
+  currentDog.setMatchStatus(false);
+  getNewDog();
+});
 
 render();
 
@@ -16,45 +24,29 @@ function render() {
 }
 
 function renderEnd() {
-    document.getElementById('card').innerHTML = 
-        ` <div = class="render-end">
+  document.getElementById('card').innerHTML = ` <div = class="render-end">
           <h1>Pups loved ${isLoved}</h1>
           <img src="images/logo.png">
       </div>    `;
-  }
+}
 
 function getNewDog() {
   if (!isWaiting) {
     isWaiting = true;
 
-  
     if (currentDog.hasBeenSwiped && currentDog.hasBeenLiked) {
       document.getElementById('like-badge').style.display = 'block';
-      isLoved++
+      isLoved++;
     } else if (currentDog.hasBeenSwiped) {
       document.getElementById('nope-badge').style.display = 'block';
-      notLoved++
+      notLoved++;
     }
+
     currentDogIndex += 1;
     currentDog = new Dog(dogsData[currentDogIndex]);
     setTimeout(() => {
-      if (currentDogIndex < dogsData.length) {
-        render();
-      } else {
-        renderEnd();
-      }
+      currentDogIndex < dogsData.length ? render() : renderEnd();
       isWaiting = false;
     }, 1500);
   }
-}
-
-
-function yes() {
-  currentDog.setMatchStatus(true);
-  getNewDog();
-}
-
-function no() {
-  currentDog.setMatchStatus(false);
-  getNewDog();
 }
